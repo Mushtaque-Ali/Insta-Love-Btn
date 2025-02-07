65f1142eed84d18e822fedc0852c691f9e512695
Mushtaque Ali <168351236+Mushtaque-Ali@users.noreply.github.com>
2025-02-07 19:44:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let con = document.querySelector("img");
+let love = document.querySelector("i");
+
+con.addEventListener("dblclick",()=> {
+    love.style = "transform: translate(-50%, -50%) scale(1);";
+    love.style.opacity = "0.8";
+
+
+    setTimeout(()=>{
+        love.style = "transform: translate(-50%, -50%) scale(0);";
+    },1000)
+    setTimeout(()=>{
+        love.style = "transform: translate(-50%, -50%) scale(0);";
+    },10000);
+})
